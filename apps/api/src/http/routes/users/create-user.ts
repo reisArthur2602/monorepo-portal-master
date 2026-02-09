fe91d0cae5e312279a62e5ea1d4e746d85a77ebb
@dev.arthur <contato.arthurreis222@gmail.com>
2026-02-09 16:24:16 -0300

    feat.

--- apps/api/src/http/routes/users/create-user.ts
+++ apps/api/src/http/routes/users/create-user.ts
@@ -1,54 +1,49 @@
-import { createUserBodySchema, createUserResponseSchema } from "@repo/schemas";
-import type { FastifyInstance } from "fastify";
-import type { ZodTypeProvider } from "fastify-type-provider-zod";
-import { hashPassword } from "../../../lib/argon2";
-import { BadRequestError } from "../../errors/bad-request";
-import { db } from "../../../db/prisma";
+import { createUserBodySchema, createUserResponseSchema } from '@repo/schemas';
+import type { FastifyInstance } from 'fastify';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { db } from '../../../db/prisma';
+import { hashPassword } from '../../../lib/argon2';
+import { BadRequestError } from '../../errors/bad-request';
 
 export const createUser = (app: FastifyInstance) =>
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .post(
-      "/create",
-      {
-        schema: {
-          tags: ["Users"],
-          summary: "Create User",
-          operationId: "createUser",
-          body: createUserBodySchema,
-          response: {
-            201: createUserResponseSchema,
-          },
-          security: [{ bearerAuth: [] }],
+    app.withTypeProvider<ZodTypeProvider>().post(
+        '/create',
+        {
+            schema: {
+                tags: ['Users'],
+                summary: 'Create User',
+                operationId: 'createUser',
+                body: createUserBodySchema,
+                response: {
+                    201: createUserResponseSchema,
+                },
+                security: [{ bearerAuth: [] }],
+            },
+
+            preHandler: [
+                  async (request) => await request.authenticate(),
+                  async (request) => await request.shouldBeAdmin(),
+            ],
         },
 
-        preHandler: [
-          async (request) => await request.authenticate(),
-          async (request) => await request.shouldBeAdmin(),
-        ],
-      },
+        async (request, reply) => {
+            const { email, name } = request.body;
 
-      async (request, reply) => {
-        const { email, name } = request.body;
+            const existingUserWithEmail = await db.user.findUnique({
+                where: { email },
+            });
 
-        const existingUserWithEmail = await db.user.findUnique({
-          where: { email },
-        });
+            if (existingUserWithEmail)
+                throw new BadRequestError('Este e-mail está sendo usado por outro membro');
 
-        if (existingUserWithEmail)
-          throw new BadRequestError(
-            "Este e-mail está sendo usado por outro membro",
-          );
+            const DEFAULT_PASSWORD_USERS = process.env.DEFAULT_PASSWORD_USERS as string;
 
-        const DEFAULT_PASSWORD_USERS = process.env
-          .DEFAULT_PASSWORD_USERS as string;
+            const passwordHash = await hashPassword(DEFAULT_PASSWORD_USERS);
 
-        const passwordHash = await hashPassword(DEFAULT_PASSWORD_USERS);
+            await db.user.create({
+                data: { email, password: passwordHash, name },
+            });
 
-        await db.user.create({
-          data: { email, password: passwordHash, name },
-        });
-
-        return reply.status(201).send({ created: true });
-      },
+            return reply.status(201).send({ created: true });
+        }
     );
